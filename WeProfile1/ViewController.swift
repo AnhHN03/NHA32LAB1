469340819177099e1f8af6a77679301386d15a29
AnhNH03 <hoanganhhadong0302@gmail.com>
2024-01-03 18:33:31 +0700

    the first commit

---
+++ WeProfile1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeProfile1
+//
+//  Created by NHA on 03/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
